a2bb41d5a80a6c95662ccf9337b14a42f051b94b
toph <taufikakbarhidayat@gmail.com>
2026-04-13 03:30:20 +0700

    feat: implement Glossy Breathing Traffic Lights (MacDots upgrade)

--- src/components/atoms/MacDots/MacDots.tsx
+++ src/components/atoms/MacDots/MacDots.tsx
@@ -4,8 +4,15 @@ import styles from './MacDots.module.css';
 export default function MacDots() {
   return (
     <div className={styles.container}>
-      {['#ff5f57', '#ffbd2e', '#28ca41'].map(color => (
-        <div key={color} className={styles.dot} style={{ background: color }} />
+      {['#ff5f57', '#ffbd2e', '#28ca41'].map((color, index) => (
+        <div 
+          key={color} 
+          className={styles.dot} 
+          style={{ 
+            '--bg': color, 
+            '--i': index 
+          } as React.CSSProperties} 
+        />
       ))}
     </div>
   );
